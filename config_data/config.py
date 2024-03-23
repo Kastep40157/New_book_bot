de968f799c6e416c6aa77e2006d6de90b43d50cf
Stepan Kaushinskii <kastep888@gmail.com>
2024-03-23 18:09:21 +1000

    config file changed. added Configuration call function

--- config_data/config.py
+++ config_data/config.py
@@ -19,28 +19,11 @@ class Config:
     tg_bot: Tg_Bot
     db: DatabaseConfig
 
-# Создаем экземпляр класса
-env: Env = Env()
-# Добавляем в переменные окружения данные, прочитанные из .env
-env.read_env()
 
-# создаем экземпляр класса Config и наполняем его данными из окружения
-config = Config(
-    tg_bot=Tg_Bot(
-        token=env("BOT_TOKEN"),
-        admins_ids=list(map(int, env.list('ADMINS_IDS')))
-    ),
-    db=DatabaseConfig(
-        db_name=env('DATABASE'),
-        db_host=env('DB_HOST'),
-        db_user=env('DB_USER'),
-        db_pass=env('DB_PASS')
-    )
-)
-print("BOT_TOKEN", config.tg_bot.token)
-print('ADMINS_IDS', config.tg_bot.admins_ids)
-print('DATABASE', config.db.db_name)
-print('DB_HOST', config.db.db_host)
-print('DB_USER', config.db.db_user)
-print('DB_PASS', config.db.db_pass)
 
+def load_config(path: str| None =None)-> Config:
+    # Создаем экземпляр класса
+    env: Env = Env()
+    # Добавляем в переменные окружения данные, прочитанные из .env
+    env.read_env(path)
+    return Config(tg_bot=Tg_Bot(token=env('BOT_TOKEN')))
