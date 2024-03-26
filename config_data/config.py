976b5d196d526e459cb3861b8040bacee99716fa
Stepan Kaushinskii <kastep888@gmail.com>
2024-03-26 08:02:04 +0700

    config file changed. del dataclass database

--- config_data/config.py
+++ config_data/config.py
@@ -5,21 +5,11 @@ from environs import Env
 @dataclass
 class Tg_Bot:
     token: str
-    admins_ids: list[int]
-
-@dataclass
-class DatabaseConfig:
-    db_name: str # Наименование базы данных
-    db_host: str # URL-адрес базы данных
-    db_user: str # Имя пользователя БД
-    db_pass: str # Пароль к БД
 
 
 @dataclass
 class Config:
     tg_bot: Tg_Bot
-    db: DatabaseConfig
-
 
 
 def load_config(path: str| None =None)-> Config:
