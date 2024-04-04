198852564cace4bc8c4a845be1a57ddaee71bcfb
Stepan Kaushinskii <kastep888@gmail.com>
2024-04-05 00:02:13 +1000

    add prepare_book func

--- services/file_handling.py
+++ services/file_handling.py
@@ -8,9 +8,28 @@ import sys
 BOOK_PATH = '../books/book.txt'
 BOOK_SIZE = 1050
 
+book: dict[int, str] = {}
+
+
+# Функция, возвращающая строку с текстом страницы и ее размер
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+
+    pass
+
+
+
 def prepare_book (path: str) -> None:
    with open(file=BOOK_PATH, mode='r') as file:
        text = file.read()
-       print(text)
+       start, page_number = 0, 1
+       while start< len(text):
+           page_text, page_size = _get_part_text(text, start, BOOK_SIZE)
+           start +=page_size
+           book[page_number] = page_text.strip()
+           page_number += 1
+
+
+
+
 
 prepare_book(BOOK_PATH)
