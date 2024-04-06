64ba6914ec4401ee4699cc2bb5d88f0ef981683e
Stepan Kaushinskii <kastep888@gmail.com>
2024-04-07 00:39:50 +1000

    add _get_part_text func

--- services/file_handling.py
+++ services/file_handling.py
@@ -13,8 +13,16 @@ book: dict[int, str] = {}
 
 # Функция, возвращающая строку с текстом страницы и ее размер
 def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
-
-    pass
+    end_point =',.!:;?'
+    last_size = size
+    if len(text) <= size + start:
+        last_size = len(text) - start
+    else:
+        for i in range (size + start - 1, start, -1):
+            if text[i] in end_point and text[i + 1] not in end_point:
+                break
+            last_size -= 1
+    return text[start: start + last_size], last_size
 
 
 
@@ -22,9 +30,9 @@ def prepare_book (path: str) -> None:
    with open(file=BOOK_PATH, mode='r') as file:
        text = file.read()
        start, page_number = 0, 1
-       while start< len(text):
+       while start < len(text):
            page_text, page_size = _get_part_text(text, start, BOOK_SIZE)
-           start +=page_size
+           start += page_size
            book[page_number] = page_text.strip()
            page_number += 1
 
@@ -32,4 +40,5 @@ def prepare_book (path: str) -> None:
 
 
 
-prepare_book(BOOK_PATH)
+
+print(prepare_book(BOOK_PATH))
