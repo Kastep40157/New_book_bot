f898b509e4a752d21252b1c850b084faec1870c1
Stepan Kaushinskii <kastep888@gmail.com>
2024-04-11 22:58:02 +1000

    add handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,17 +1,57 @@
+from copy import deepcopy
 from aiogram import F, Router
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from lexicon.lexicon_ru import LEXICON
 from keyboards.pagination_kb import create_pagination_keyboard
+from database.database import users_db, user_dict_template
+from services.file_handling import book
+
 router = Router()
 
 
-@router.message(CommandStart)
+# Этот хэндлер будет срабатывать на команду "/start" -
+# добавлять пользователя в базу данных, если его там еще не было
+# и отправлять ему приветственное сообщение
+@router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(LEXICON[message.text])
+    if message.from_user.id not in users_db:
+        users_db[message.from_user.id] = deepcopy(user_dict_template)
 
-@router.message (Command)
+
+@router.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(LEXICON[message.text])
 
 
+# Этот хэндлер будет срабатывать на команду "/beginning"
+# и отправлять пользователю первую страницу книги с кнопками пагинации
+@router.message(Command(commands='beginning'))
+async def process_beginning_command(message: Message):
+    users_db[message.from_user.id]['page'] = 1
+    text = book[users_db[message.from_user.id]['page']]
+    await message.answer(
+        text=text,
+        reply_markup=create_pagination_keyboard(
+            'backward',
+            f'{users_db[message.from_user.id]["page"]}/{len(book)}',
+            'forward'
+        )
+    )
+
+
+# Этот хендлер будет срабатывать на команду  / continue
+# и отправлять пользователя на страницу на которой он остановился
+#  при взаимодествии с ботом
+@router.message(Command(commands='continue'))
+async def process_continue_command(message:Message):
+    text = book[users_db[message.from_user.id]['page']]
+    await message.answer(
+        text=text,
+        reply_markup=create_pagination_keyboard(
+            'backward',f'{users_db[message.from_user.id]['page']}/{len(book)}',
+            'forward'
+        )
+
+    )
